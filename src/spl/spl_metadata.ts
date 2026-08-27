900c0171baeda24e33484d8c91c0257dad8bcb06
lightx0x <kingcairo4000@gmail.com>
2026-08-27 22:43:11 +0100

    Create Metadata Account

--- src/spl/spl_metadata.ts
+++ src/spl/spl_metadata.ts
@@ -1,20 +1,20 @@
 import {
-  createSignerFromKeypair,
-  publicKey,
-  signerIdentity,
+	createSignerFromKeypair,
+	publicKey,
+	signerIdentity,
 } from "@metaplex-foundation/umi";
 import wallet from "../../devnet-wallet.json";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import {
-  createMetadataAccountV3,
-  CreateMetadataAccountV3InstructionAccounts,
-  CreateMetadataAccountV3InstructionArgs,
-  DataV2Args,
+	createMetadataAccountV3,
+	CreateMetadataAccountV3InstructionAccounts,
+	CreateMetadataAccountV3InstructionArgs,
+	DataV2Args,
 } from "@metaplex-foundation/mpl-token-metadata";
 import bs58 from "bs58";
 
 //paste your mint address got from spl_init.ts
-const mint = publicKey("E2Jazz2VXcVL9RZkn6ZFA4q1YGvgEvrns3Gr6w72DC4w");
+const mint = publicKey("2T1avQ2u7LxNWA4AmFXCmo5o2PPUsWiubKxUgVxAtLXc");
 
 const umi = createUmi("https://api.devnet.solana.com");
 
@@ -24,27 +24,39 @@ const signer = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(signer));
 
 (async () => {
-  try {
-    const accounts: CreateMetadataAccountV3InstructionAccounts = {
-      mint,
-      mintAuthority: signer,
-    };
+	try {
+		const accounts: CreateMetadataAccountV3InstructionAccounts = {
+			mint,
+			mintAuthority: signer,
+		};
 
-    //change the metadata
-    // const data: DataV2Args =
+		//change the metadata
+		const data: DataV2Args = {
+			name: "Tux Tux Coin",
+			symbol: "TXC",
+			uri: "",
+			sellerFeeBasisPoints: 0,
+			creators: null,
+			collection: null,
+			uses: null,
+		};
 
-    // const args: CreateMetadataAccountV3InstructionArgs =
+		const args: CreateMetadataAccountV3InstructionArgs = {
+			data,
+			isMutable: true,
+			collectionDetails: null,
+		};
 
-    // const tx = createMetadataAccountV3(umi, {
-    //   ...accounts,
-    //   ...args,
-    // });
+		const tx = createMetadataAccountV3(umi, {
+			...accounts,
+			...args,
+		});
 
-    // const result = await tx.sendAndConfirm(umi);
-    // console.log("signature: ", bs58.encode(Buffer.from(result.signature)));
-  } catch (error) {
-    console.log("error", error);
-  }
+		const result = await tx.sendAndConfirm(umi);
+		console.log("signature: ", bs58.encode(Buffer.from(result.signature)));
+	} catch (error) {
+		console.log("error", error);
+	}
 })();
 
-//43ttSnN9qaVi8TDcWwBZo5mUbfKDXY8d1N7exdJojJxV7qjKuwXoEh7qASXbFU4QFrAEFzZvcmWpRch434hSVNLN
+//3iiCtTtsAbuDKd46E3iPcQYvvvMcrXJpUKjYwyHLiGuXEZA5rw7zyBRLFjUN6sB3pGvGvnjMRcMFvbWUMnsGPZp7
