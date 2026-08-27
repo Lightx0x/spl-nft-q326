44fa2401ebce0d25ac0d5181485fe0ee282f3362
lightx0x <kingcairo4000@gmail.com>
2026-08-27 14:56:50 +0100

    Generate Mint Account

--- src/spl/spl_init.ts
+++ src/spl/spl_init.ts
@@ -1,23 +1,23 @@
 import {
-  appendTransactionMessageInstruction,
-  appendTransactionMessageInstructions,
-  assertIsTransactionMessageWithBlockhashLifetime,
-  assertIsTransactionWithBlockhashLifetime,
-  createKeyPairSignerFromBytes,
-  createSolanaRpc,
-  createSolanaRpcSubscriptions,
-  createTransactionMessage,
-  generateKeyPairSigner,
-  getSignatureFromTransaction,
-  sendAndConfirmTransactionFactory,
-  setTransactionMessageFeePayerSigner,
-  setTransactionMessageLifetimeUsingBlockhash,
-  signTransactionMessageWithSigners,
+	appendTransactionMessageInstruction,
+	appendTransactionMessageInstructions,
+	assertIsTransactionMessageWithBlockhashLifetime,
+	assertIsTransactionWithBlockhashLifetime,
+	createKeyPairSignerFromBytes,
+	createSolanaRpc,
+	createSolanaRpcSubscriptions,
+	createTransactionMessage,
+	generateKeyPairSigner,
+	getSignatureFromTransaction,
+	sendAndConfirmTransactionFactory,
+	setTransactionMessageFeePayerSigner,
+	setTransactionMessageLifetimeUsingBlockhash,
+	signTransactionMessageWithSigners,
 } from "@solana/kit";
 import {
-  getInitializeMintInstruction,
-  getMintSize,
-  TOKEN_PROGRAM_ADDRESS,
+	getInitializeMintInstruction,
+	getMintSize,
+	TOKEN_PROGRAM_ADDRESS,
 } from "@solana-program/token";
 import { getCreateAccountInstruction } from "@solana-program/system";
 
@@ -27,12 +27,60 @@ import wallet from "../../devnet-wallet.json";
 const rpc = createSolanaRpc("https://api.devnet.solana.com");
 
 const rpcSubscriptions = createSolanaRpcSubscriptions(
-  "wss://api.devnet.solana.com",
+	"wss://api.devnet.solana.com",
 );
 
 (async () => {
-  try {
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const signer = await createKeyPairSignerFromBytes(new Uint8Array(wallet));
+		const mint = await generateKeyPairSigner();
+
+		const space = getMintSize();
+		const rent = await rpc
+			.getMinimumBalanceForRentExemption(BigInt(space))
+			.send();
+
+		const createAccountIx = getCreateAccountInstruction({
+			payer: signer,
+			newAccount: mint,
+			lamports: rent,
+			space,
+			programAddress: TOKEN_PROGRAM_ADDRESS,
+		});
+
+		const initMintIx = getInitializeMintInstruction({
+			mint: mint.address,
+			decimals: 6,
+			mintAuthority: signer.address,
+		});
+
+		const { value: latestBlockhash } = await rpc.getLatestBlockhash().send();
+
+		const msg = createTransactionMessage({ version: 0 });
+		const msgWithPayer = setTransactionMessageFeePayerSigner(signer, msg);
+		const msgWithLifetime = setTransactionMessageLifetimeUsingBlockhash(
+			latestBlockhash,
+			msgWithPayer,
+		);
+		const txMessage = appendTransactionMessageInstructions(
+			[createAccountIx, initMintIx],
+			msgWithLifetime,
+		);
+
+		const signedTx = await signTransactionMessageWithSigners(txMessage);
+		assertIsTransactionWithBlockhashLifetime(signedTx);
+
+		const sendAndConfirm = sendAndConfirmTransactionFactory({
+			rpc,
+			rpcSubscriptions,
+		});
+
+		await sendAndConfirm(signedTx, { commitment: "confirmed" });
+
+		const signature = getSignatureFromTransaction(signedTx);
+		console.log(`Mint address: ${mint.address}`);
+		console.log(`Tx signature: ${signature}`);
+	} catch (error) {
+		console.log(error);
+	}
 })();
