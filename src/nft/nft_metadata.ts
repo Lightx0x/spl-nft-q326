007464b9380074bc69b3d8a64f1092af7ef636b2
lightx0x <kingcairo4000@gmail.com>
2026-08-28 16:27:00 +0100

    Mint NFT

--- src/nft/nft_metadata.ts
+++ src/nft/nft_metadata.ts
@@ -1,38 +1,46 @@
 import {
-  createSignerFromKeypair,
-  signerIdentity,
+	createSignerFromKeypair,
+	signerIdentity,
 } from "@metaplex-foundation/umi";
 import wallet from "../../devnet-wallet.json";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
 
 const umi = createUmi(
-  process.env.SOLANA_RPC_URL ?? "https://api.devnet.solana.com",
+	process.env.SOLANA_RPC_URL ?? "https://api.devnet.solana.com",
 );
 
 const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 
 umi.use(
-  irysUploader({
-    address: "https://devnet.irys.xyz/",
-  }),
+	irysUploader({
+		address: "https://devnet.irys.xyz/",
+	}),
 );
 
 umi.use(signerIdentity(signer));
 
 (async () => {
-  try {
-    //change the image uri to your image uri obtained from nft_image.ts
-    const image =
-      "https://gateway.irys.xyz/5EDyiNrMWfhjdsEwXLrwkHPwZoZB2m1A2Kudrfxo1tpr";
+	try {
+		//change the image uri to your image uri obtained from nft_image.ts
+		const image =
+			"https://gateway.irys.xyz/oMKAcLb21uo64yZUQmBsrXd7KVfbsyiYME2rU1gHB3G";
 
-    //json scheme : https://www.metaplex.com/docs/smart-contracts/core/json-schema
-    //change the metadata
-    // const metadata =
-    // const myUri =
-    // console.log(`metadata uri: ${myUri} `);
-  } catch (error) {
-    console.log("error", error);
-  }
+		//json scheme : https://www.metaplex.com/docs/smart-contracts/core/json-schema
+		//change the metadata
+		const metadata = {
+			name: "Tux Tux",
+			symbol: "TUX",
+			description: "A Tux NFT",
+			image,
+			properties: {
+				files: [{ type: "image/png", uri: image }],
+			},
+		};
+		const myUri = await umi.uploader.uploadJson(metadata);
+		console.log(`metadata uri: ${myUri}`);
+	} catch (error) {
+		console.log("error", error);
+	}
 })();
--- src/nft/nft_mint.ts
+++ src/nft/nft_mint.ts
@@ -1,15 +1,15 @@
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import wallet from "../../devnet-wallet.json";
 import {
-  createSignerFromKeypair,
-  generateSigner,
-  signerIdentity,
+	createSignerFromKeypair,
+	generateSigner,
+	signerIdentity,
 } from "@metaplex-foundation/umi";
 import { create, mplCore } from "@metaplex-foundation/mpl-core";
 import { base58 } from "@metaplex-foundation/umi/serializers";
 
 const umi = createUmi(
-  process.env.SOLANA_RPC_URL ?? "https://api.devnet.solana.com",
+	process.env.SOLANA_RPC_URL ?? "https://api.devnet.solana.com",
 );
 
 const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
@@ -20,18 +20,21 @@ umi.use(signerIdentity(signer));
 umi.use(mplCore());
 
 (async () => {
-  try {
-    const metadataUri =
-      "https://gateway.irys.xyz/BihKZnhMCvxN3i34cv25eEyFgUvBVGJQn1Gp11D4LxEi ";
-    const asset = generateSigner(umi);
+	try {
+		const metadataUri =
+			"https://gateway.irys.xyz/94RAp3gEMbGaijwogbYnV6fc2ac5T71AyxYhTbS7mnv1";
+		const asset = generateSigner(umi);
 
-    //add you nft name and metadata uri
-    // const tx = await create()
+		//add you nft name and metadata uri
+		const tx = await create(umi, {
+			asset,
+			name: "Tux Tux",
+			uri: metadataUri,
+		}).sendAndConfirm(umi);
 
-    // const signature = base58.deserialize(tx.signature)[0];
-
-    // console.log(`signature ${signature} , asset : ${asset.publicKey}`);
-  } catch (e) {
-    console.log(`errior ${e}`);
-  }
+		const signature = base58.deserialize(tx.signature)[0];
+		console.log(`signature ${signature} , asset : ${asset.publicKey}`);
+	} catch (e) {
+		console.log(`error ${e}`);
+	}
 })();
